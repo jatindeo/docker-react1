1a231faf3e578241793913fc73e7ea6dc5384a1a
JatindraSinghDeo <jatindra.singhdeo@gmail.com>
2022-07-13 09:57:55 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React Hi There 5!
         </a>
       </header>
     </div>
